459d38afe7f4fb3cbba2b7925c48650f60f6f535
Laurenskis <laurynas867@gmail.com>
2024-08-18 15:21:12 +0000

    asdsadasads

--- main.py
+++ main.py
@@ -11,56 +11,32 @@ system_manager = SystemManager()
 bash_manager = BashScriptManager()
 printer = CustomPrinter() 
 
-printer.print("Starting script...", level="INFO", color="blue")
+username = 'laurenskis'
 
-# Install Zsh
-printer.print("Installing Zsh...", level="INFO", color="blue")
-stdout, stderr = system_manager.install_package('zsh')
-printer.print(f"Zsh Installation: {stdout}", level="INFO", color="green")
-if stderr:
-    printer.print(f"Error installing Zsh: {stderr}", level="ERROR", color="red")
 
-# Install Oh My Zsh
-printer.print("Installing Oh My Zsh...", level="INFO", color="blue")
-ohmyzsh_install_script = 'https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh'
-stdout, stderr = CommandRunner.run(f"sh -c \"$(curl -fsSL {ohmyzsh_install_script})\"")
-printer.print(f"Oh My Zsh Installation: {stdout}", level="INFO", color="green")
-if stderr:
-    printer.print(f"Error installing Oh My Zsh: {stderr}", level="ERROR", color="red")
 
-# Update .zshrc with a custom configuration (optional)
-zshrc_file = os.path.expanduser('~/.zshrc')
-custom_zshrc_content = '''
-# Custom Zsh configuration
+CommandRunner.run("termux-change-repo")
+CommandRunner.run("pkg update -y -o Dpkg::Options::='--force-confold'")
+CommandRunner.run("pkg upgrade -y -o Dpkg::Options::='--force-confold'")
+CommandRunner.run("sed -i '12s/^#//' $HOME/.termux/termux.properties")
+CommandRunner.run("termux-setup-storage")
 
-# Path to Oh My Zsh
-export ZSH="$HOME/.oh-my-zsh"
+pkgs = ['wget', 'python', 'ncurses-utils', 'dbus', 'proot-distro', 'x11-repo', 'tur-repo', 'pulseaudio']
 
-# Set the theme
-ZSH_THEME="agnoster"
+CommandRunner.run("pkg uninstall dbus -y")
+CommandRunner.run("pkg update")
+CommandRunner.run(f"pkg install {' '.join(pkgs)} -y -o Dpkg::Options::='--force-confold'")
 
-# Enable plugins
-plugins=(git z)
+# Create default directories
+os.makedirs("Desktop", exist_ok=True)
+os.makedirs("Downloads", exist_ok=True)
 
-# Source Oh My Zsh
-source $ZSH/oh-my-zsh.sh
 
-# Custom aliases
-alias ll='ls -la'
-alias gs='git status'
-'''
+CommandRunner.run("wget -N https://github.com/L4ur3nsk1s/Termux-XFCE-Custom/raw/main/xfce.py")
+CommandRunner.run("wget -N https://github.com/L4ur3nsk1s/Termux-XFCE-Custom/raw/main/proot.py")
+CommandRunner.run("wget -N https://github.com/L4ur3nsk1s/Termux-XFCE-Custom/raw/main/utils.py")
+CommandRunner.run("chmod +x *.py")
 
-# Create or overwrite the .zshrc file
-printer.print("Updating .zshrc with custom configuration...", level="INFO", color="blue")
-zshrc_file_path = file_manager.create_file(zshrc_file, content=custom_zshrc_content, overwrite=True)
-printer.print(f"Updated .zshrc File: {zshrc_file_path}", level="INFO", color="green")
-
-# Change the default shell to Zsh
-printer.print("Changing default shell to Zsh...", level="INFO", color="blue")
-stdout, stderr = CommandRunner.run('chsh -s $(which zsh)')
-printer.print(f"Change Default Shell to Zsh: {stdout}", level="INFO", color="green")
-if stderr:
-    printer.print(f"Error changing default shell: {stderr}", level="ERROR", color="red")
-
-# Inform the user
-printer.print("Zsh and Oh My Zsh setup complete. Please restart your terminal or log out and log back in to apply changes.", level="COMPLETE", color="green")
+CommandRunner.run(f"./xfce.py {username}")
+CommandRunner.run(f"./proot.py {username}")
+CommandRunner.run("./utils.py")
