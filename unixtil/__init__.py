f94842920a4d7edb131edd8e98176f413707c9b2
L4ur3nsk1s <laurynas867@gmail.com>
2024-09-23 03:35:49 +0300

    sadasd

--- unixtil/__init__.py
+++
@@ -1,7 +0,0 @@
-from .runner import CommandRunner
-from .file_manager import FileManager
-from .bash_manager import BashScriptManager
-from .downloader import Downloader
-from .logger import Logger
-from .system_manager import SystemManager
-from .printer import CustomPrinter
--- unixtil/runner.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-class CommandRunner:
-    @staticmethod
-    def run(command):
-        result = subprocess.run(command, shell=True, capture_output=True, text=True)
-        return result.stdout.strip(), result.stderr.strip() if result.stderr else None
