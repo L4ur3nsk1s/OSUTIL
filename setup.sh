dc72f3b7d4719d38e41f35f4df968a012329cb9b
L4ur3nsk1s <laurynas867@gmail.com>
2024-10-01 13:33:26 +0300

    fixes

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 sudo apt update && sudo apt upgrade -y
 
 # Install essential packages
-sudo apt install -y git wget zsh bat eza nala
+sudo apt install -y git wget zsh bat eza nala gh
 
 # Install development tools
 sudo apt install -y golang rustc nodejs python3 python3-pip php
