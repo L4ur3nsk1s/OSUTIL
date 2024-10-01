b11612448d37d26c62cc047285099574759109d1
L4ur3nsk1s <laurynas867@gmail.com>
2024-10-01 13:33:06 +0300

    some fixes

--- scripts/nvim.sh
+++ scripts/nvim.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-
-sudo apt update && sudo apt upgrade -y
-sudo apt install ripgrep -y
+apt update && sudo apt upgrade -y
+apt install ripgrep -y
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
+rm -rf /opt/nvim
+tar -C /opt -xzf nvim-linux64.tar.gz
 echo "export PATH="$PATH:/opt/nvim-linux64/bin"" >>~/.zshrc
 source ~/.zshrc
 git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
-sudo apt install ripgrep -y
--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
-apt update
-apt upgrade
-apt install git wget
-apt install zsh bat eza nala
-apt install golang rustc
-apt install php 
+#!/bin/bash
+
+# Update and upgrade the system
+sudo apt update && sudo apt upgrade -y
+
+# Install essential packages
+sudo apt install -y git wget zsh bat eza nala
+
+# Install development tools
+sudo apt install -y golang rustc nodejs python3 python3-pip php
