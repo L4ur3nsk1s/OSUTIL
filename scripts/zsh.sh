8c0289d76b6d9a2d3bff426844703ff272e99bbd
Laurenskis <laurynas867@gmail.com>
2025-02-08 13:22:42 +0200

    Update zsh.sh

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -1,16 +1,57 @@
 #!/bin/bash
-sudo apt update && sudo apt upgrade -y
-sudo apt install zsh -y
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh > ~/.oh-my-zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/git/git.plugin.zsh > ~/.oh-my-zsh/plugins/git/git.plugin.zsh
-curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/extract/extract.plugin.zsh > ~/.oh-my-zsh/plugins/extract/extract.plugin.zsh
-curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh > ~/.oh-my-zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-echo "source ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
-echo "ZSH_THEME=\"powerlevel10k/powerlevel10k\"" >>~/.zshrc
-echo "source ~/.oh-my-zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >>~/.zshrc
-echo "plugins=(git zsh-syntax-highlighting zsh-autosuggestions git)" >>~/.zshrc
-echo "alias zshconfig=\"nvim ~/.zshrc\"" >>~/.zshrc
-echo "alias ohmyzsh=\"nvim ~/.oh-my-zsh\"" >>~/.zshrc
-source ~/.zshrc
+
+# Colors for output
+GREEN="\e[32m"
+YELLOW="\e[33m"
+RESET="\e[0m"
+
+# Function to check if script is run as root
+check_root() {
+    if [[ $EUID -ne 0 ]]; then
+        echo -e "${YELLOW}This script must be run as root. Please run with sudo.${RESET}"
+        exit 1
+    fi
+}
+
+# Update and upgrade system
+update_system() {
+    echo -e "${GREEN}Updating and upgrading the system...${RESET}"
+    apt update && apt upgrade -y
+}
+
+# Install Zsh & Oh My Zsh
+install_zsh() {
+    echo -e "${GREEN}Installing Zsh and Oh My Zsh...${RESET}"
+    apt install -y zsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+}
+
+# Install plugins and Powerlevel10k
+install_plugins() {
+    echo -e "${GREEN}Installing Oh My Zsh plugins and Powerlevel10k...${RESET}"
+    
+    ZSH_CUSTOM=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
+    
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+    git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+    git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+    # Update .zshrc
+    echo "ZSH_THEME=\"powerlevel10k/powerlevel10k\"" >> ~/.zshrc
+    echo "plugins=(git zsh-syntax-highlighting zsh-autosuggestions)" >> ~/.zshrc
+    echo "alias zshconfig=\"nvim ~/.zshrc\"" >> ~/.zshrc
+    echo "alias ohmyzsh=\"nvim ~/.oh-my-zsh\"" >> ~/.zshrc
+}
+
+# Main script execution
+main() {
+    check_root
+    update_system
+    install_zsh
+    install_plugins
+
+    echo -e "${GREEN}Installation complete! Restart your terminal or run 'exec zsh' to apply changes.${RESET}"
+}
+
+# Run the main function
+main
