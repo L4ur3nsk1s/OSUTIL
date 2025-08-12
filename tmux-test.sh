3402dc1b6840647f0eab3f20191bd1409f8dad33
Laurenskis <laurynas867@gmail.com>
2025-08-13 00:31:07 +0300

    s

--- tmux-test.sh
+++ tmux-test.sh
@@ -50,10 +50,20 @@ set -g history-limit 10000            # Scrollback buffer size
 # Status bar style and theme
 set -g status-bg colour235
 set -g status-fg colour136
-set -g status-left-length 40
-set -g status-right-length 100
-set -g status-left '#[fg=green]#H #[fg=yellow]#(whoami)'
-set -g status-right '#(date +"%Y-%m-%d %H:%M") #[fg=cyan]#(uptime | cut -d "," -f 1)'
+set -g status-interval 5                      # Refresh every 5 seconds
+
+set -g status-left-length 60
+set -g status-right-length 150
+
+# Status left: hostname and user with colors
+set -g status-left '#[fg=green,bold]#H #[fg=yellow]#(whoami) '
+
+# Status right: date/time, battery, CPU load, network, git branch
+set -g status-right '#[fg=cyan]#(date +"%Y-%m-%d %H:%M") ' \
+                    '#[fg=colour39]#{battery_percentage}%% #[fg=colour241]⚡ ' \
+                    '#[fg=colour45]#(uptime | cut -d "," -f 1) ' \
+                    '#[fg=colour208]#(if [ -d .git ]; then git branch --show-current 2>/dev/null; fi) ' \
+                    '#[fg=colour39]#(ifconfig $(ip route get 8.8.8.8 | awk '\''NR==1{print $5}'\'') 2>/dev/null | grep "inet " | awk '\''{print $2}'\'') '
 
 # Plugins
 set -g @plugin 'tmux-plugins/tpm'                     # TPM itself
@@ -62,8 +72,15 @@ set -g @plugin 'tmux-plugins/tmux-resurrect'          # Save/restore sessions
 set -g @plugin 'tmux-plugins/tmux-continuum'          # Automatic restore and saving
 set -g @plugin 'tmux-plugins/tmux-prefix-highlight'   # Highlights prefix key
 
+# Additional useful plugins:
+set -g @plugin 'tmux-plugins/tmux-copycat'            # Enhanced searching
+set -g @plugin 'tmux-plugins/tmux-yank'               # Clipboard integration
+set -g @plugin 'tmux-plugins/tmux-open'               # Open highlighted URLs/files
+set -g @plugin 'tmux-plugins/tmux-cpu'                # Show CPU load in status bar
+
 # Initialize TPM (keep this line at the very bottom)
 run '~/.tmux/plugins/tpm/tpm'
+
 EOF
 }
 
